dac590ee4bfdb9d398232c8b4aaf56ae5de58ee3
santi <santi@LAPTOP-JVLOSMND>
2018-09-24 20:16:49 -0500

    Librería Ruiz

--- src/Ejecutable.java
+++ src/Ejecutable.java
@@ -12,32 +12,7 @@ import java.util.*;
 public class Ejecutable {
     Scanner bf = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        Libreria Ruiz = new Libreria();
     }
-    public void addCotizacion(){
-        int descuento = bf.nextInt();
-        Cotizacion cot = new Cotizacion(descuento);
-        int opcion;
-        do{
-            
-            System.out.println("Titulo: ");
-            String titulo = bf.next();
-            System.out.println("Editorial: ");
-            String editorial = bf.next();
-            System.out.println("Categoria: ");
-            String categoria = bf.next();
-            System.out.println("Tipo: ");
-            String tipo = bf.next();
-            System.out.println("Estante: ");
-            int estante = bf.nextInt();
-            System.out.println("Precio: ");
-            int precio = bf.nextInt();
-            Libro l = new Libro(titulo, editorial, categoria, tipo, estante, precio);
-            cot.addLibro(l);
-            opcion = bf.nextInt();
-            System.out.println("1. Agregar a la cotización");
-            System.out.println("0. Salir");
-        }while(opcion != 0);
-        System.out.println("Valor cotizacion: " + cot.calcularTotal());
-    }
+    
 }
